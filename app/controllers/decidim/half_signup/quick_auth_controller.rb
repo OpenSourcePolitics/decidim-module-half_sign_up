96f76509ec672c20e112a1a2fcc0e4b6721362ac
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-02-23 13:03:25 +0200

    rewise I18n.t links

--- app/controllers/decidim/half_signup/quick_auth_controller.rb
+++ app/controllers/decidim/half_signup/quick_auth_controller.rb
@@ -6,17 +6,16 @@ module Decidim
       include Decidim::HalfSignup::QuickAuth::AuthSessionHandler
       include Decidim::HalfSignup::PartialSignupSettings
 
-      before_action :store_user_location!, if: :storable_location?
-      # TODO: Ensure user is not logged in
+      before_action :ensure_authorized, only: [:sms, :email, :verify, :options]
 
       def sms
-        ensure_authorized("sms")
+        ensure_enabled_auth("sms")
         @form = form(SmsAuthForm).instance
         init_sessions!({ auth_method: "sms" })
       end
 
       def email
-        ensure_authorized("email")
+        ensure_enabled_auth("email")
         @form = form(EmailAuthForm).instance
         init_sessions!({ auth_method: "email" })
       end
@@ -30,10 +29,10 @@ module Decidim
           on(:ok) do |result|
             if sms_auth?
               update_sessions!({ code: result, country: @form.phone_country, phone: @form.phone_number })
-              flash[:notice] = I18n.t(".success", scope: "decidim.half_signup.quick_auth.sms_verification", phone: formatted_phone_number(@form))
+              flash[:notice] = I18n.t("success", scope: "decidim.half_signup.quick_auth.sms_verification", phone: formatted_phone_number(@form))
             else
               update_sessions!({ code: result, email: @form.email })
-              flash[:notice] = I18n.t(".success", scope: "decidim.half_signup.quick_auth.email_verification", email: form.email)
+              flash[:notice] = I18n.t("success", scope: "decidim.half_signup.quick_auth.email_verification", email: form.email)
             end
             redirect_to action: "verify"
           end
@@ -42,7 +41,7 @@ module Decidim
             flash.now[:alert] = if error_code
                                   sms_sending_error(error_code)
                                 else
-                                  I18n.t(".unknown", scope: "decidim.half_signup.quick_auth.sms_verification")
+                                  I18n.t("unknown", scope: "decidim.half_signup.quick_auth.sms_verification")
                                 end
             if sms_auth?
               render action: "sms"
@@ -59,17 +58,17 @@ module Decidim
       end
 
       def authenticate
-        @form = form(VerificationCodeForm).from_params(params)
+        @form = form(VerificationCodeForm).from_params(params.merge(current_locale: current_locale))
         @verification_code = auth_session["code"]
         AuthenticateUser.call(form: @form, data: auth_session) do
           on(:ok) do |user|
-            flash[:notice] = I18n.t(".signed_in", scope: "decidim.half_signup.quick_auth.authenticate_user")
+            flash[:notice] = I18n.t("signed_in", scope: "decidim.half_signup.quick_auth.authenticate_user")
             reset_auth_session
-            sign_in_and_redirect user
+            sign_in_and_redirect user, event: :authentication
           end
 
           on(:invalid) do
-            flash.now[:error] = I18n.t(".error", scope: "decidim.half_signup.quick_auth.authenticate_user")
+            flash.now[:error] = I18n.t("error", scope: "decidim.half_signup.quick_auth.authenticate_user")
             render :verify
           end
         end
@@ -84,9 +83,9 @@ module Decidim
           on(:ok) do |result|
             update_sessions!(result)
             flash[:notice] = if sms_auth?
-                               I18n.t(".success", scope: "decidim.half_signup.quick_auth.sms_verification", phone: formatted_phone_number(@form))
+                               I18n.t("success", scope: "decidim.half_signup.quick_auth.sms_verification", phone: formatted_phone_number(@form))
                              else
-                               I18n.t(".success", scope: "decidim.half_signup.quick_auth.email_verification", email: auth_session["email"])
+                               I18n.t("success", scope: "decidim.half_signup.quick_auth.email_verification", email: auth_session["email"])
                              end
             redirect_to action: "verify"
           end
@@ -96,7 +95,7 @@ module Decidim
               flash.now[:alert] = sms_sending_error(error_code)
               render action: "sms"
             else
-              flash.now[:alert] = I18n.t(".error", scope: "decidim.half_signup.quick_auth.email_verification")
+              flash.now[:alert] = I18n.t("error", scope: "decidim.half_signup.quick_auth.email_verification")
               render action: "email"
             end
           end
@@ -107,37 +106,31 @@ module Decidim
 
       private
 
-      def store_user_location!
-        # :user is the scope we are authenticating
-        store_location_for(:user, request.fullpath)
-      end
-
-      # Its important that the location is NOT stored if:
-      # - The request method is not GET (non idempotent)
-      # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
-      #    infinite redirect loop.
-      # - The request is an Ajax request as this can lead to very unexpected behaviour.
-      def storable_location?
-        request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
-      end
-
-      def ensure_authorized(option)
+      def ensure_enabled_auth(option)
         return if half_signup_handlers.include? option
 
-        flash[:error] = I18n.t("decidim.half_signup.quick_auth.options.not_allowed")
+        flash[:error] = I18n.t("not_allowed", scope: "decidim.half_signup.quick_auth.options")
         redirect_to decidim_half_signup.users_quick_auth_path
+      end
+
+      def ensure_authorized
+        return true if current_user.blank?
+
+        flash[:error] = I18n.t("not_allowed", scope: "decidim.half_signup.quick_auth.options")
+        redirect_to decidim.root_path
+        false
       end
 
       def sms_sending_error(error_code)
         case error_code
         when :invalid_to_number
-          I18n.t(".invalid_to_number", scope: "decidim.half_signup.quick_auth.sms_verification")
+          I18n.t("invalid_to_number", scope: "decidim.half_signup.quick_auth.sms_verification")
         when :invalid_geo_permission
-          I18n.t(".invalid_geo_permission", scope: "decidim.half_signup.quick_auth.sms_verification")
+          I18n.t("invalid_geo_permission", scope: "decidim.half_signup.quick_auth.sms_verification")
         when :invalid_from_number
-          I18n.t(".invalid_from_number", scope: "decidim.half_signup.quick_auth.sms_verification")
+          I18n.t("invalid_from_number", scope: "decidim.half_signup.quick_auth.sms_verification")
         else
-          I18n.t(".unknown", scope: "decidim.half_signup.quick_auth.sms_verification")
+          I18n.t("unknown", scope: "decidim.half_signup.quick_auth.sms_verification")
         end
       end
 
@@ -157,7 +150,7 @@ module Decidim
       def ensure_code_delivery
         return true if send_code_allowed?
 
-        flash[:error] = I18n.t(".not_allowed", scope: "decidim.half_signup.quick_auth.resend_code")
+        flash[:error] = I18n.t("not_allowed", scope: "decidim.half_signup.quick_auth.resend_code")
         redirect_to action: "verify"
         false
       end
