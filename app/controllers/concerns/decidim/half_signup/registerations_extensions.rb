2d1669d273d0b14fe5c754317bde97949a35c347
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-02-28 18:16:24 +0200

    Fix a bug in the registerations_extension file

--- app/controllers/concerns/decidim/half_signup/registerations_extensions.rb
+++ app/controllers/concerns/decidim/half_signup/registerations_extensions.rb
@@ -12,6 +12,9 @@ module Decidim
           if half_signup_enabled?(current_organization)
             redirect_to decidim_half_signup.users_quick_auth_path
           else
+            @form = form(::Decidim::RegistrationForm).from_params(
+              user: { sign_up_as: "user" }
+            )
             super
           end
         end
