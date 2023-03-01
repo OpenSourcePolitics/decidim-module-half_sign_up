08292fce69e81865be7bd853587ec7ed8405f812
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-03-01 14:14:28 +0200

    Refactor migrations

--- db/migrate/20230214091207_create_auth_settings.rb
+++ db/migrate/20230214091207_create_auth_settings.rb
@@ -3,8 +3,9 @@
 class CreateAuthSettings < ActiveRecord::Migration[6.1]
   def change
     create_table :decidim_half_signup_auth_settings do |t|
-      t.boolean :enable_partial_sms_signup_verification, default: false
-      t.boolean :enable_partial_email_signup_verification, default: false
+      t.boolean :enable_partial_sms_signup, default: false
+      t.boolean :enable_partial_email_signup, default: false
+      t.string :slug
       t.references :decidim_organization, foreign_key: true, index: { name: :index_half_signup_auth_settings_on_organization_id }
 
       t.timestamps
--- db/migrate/20230214140801_add_slug_to_decidim_auth_settings.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddSlugToDecidimAuthSettings < ActiveRecord::Migration[6.1]
-  def change
-    add_column :decidim_half_signup_auth_settings, :slug, :string
-  end
-end
