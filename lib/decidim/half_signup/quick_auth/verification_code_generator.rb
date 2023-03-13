ebeafe5576190d3f992c1d7c3684a0246a930a05
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-03-13 17:52:16 +0200

    generate verification code based on the verification length configuration

--- lib/decidim/half_signup/quick_auth/verification_code_generator.rb
+++ lib/decidim/half_signup/quick_auth/verification_code_generator.rb
@@ -5,7 +5,11 @@ module Decidim
     module QuickAuth
       module VerificationCodeGenerator
         def generate_code
-          SecureRandom.random_number(10_000_000).to_s
+          SecureRandom.random_number(10**auth_code_length).to_s
+        end
+
+        def auth_code_length
+          ::Decidim::HalfSignup.auth_code_length
         end
       end
     end
