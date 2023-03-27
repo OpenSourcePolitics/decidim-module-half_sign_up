8741faaa173f0241b4c5d4c7759f63825139f437
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-03-27 15:54:02 +0300

    Add tests for the configurations that is added

--- spec/system/manage_email_sign_in_and_sing_up_spec.rb
+++ spec/system/manage_email_sign_in_and_sing_up_spec.rb
@@ -6,6 +6,9 @@ describe "Manage sms sign in", type: :system do
   let(:organization) { create(:organization) }
   let!(:auth_settings) { create(:auth_setting, organization: organization, enable_partial_email_signup: true) }
   let(:decidim_half_signup) { Decidim::HalfSignup::Engine.routes.url_helpers }
+  let(:email) { "someone@example.org" }
+  let!(:user) { create(:user, :confirmed, email: "anotherone@example.org", organization: auth_settings.organization) }
+  let(:code) { "1234" }
 
   before do
     switch_to_host(organization.host)
@@ -13,53 +16,48 @@ describe "Manage sms sign in", type: :system do
   end
 
   describe "Sign in/Sign up process" do
-    let(:email) { "someone@example.org" }
-
     before do
       click_link "Sign In"
     end
 
     context "when the account does not exist" do
-      context "when enter email" do
-        before { click_link "TO YOUR EMAIL" }
+      before do
+        allow(SecureRandom).to receive(:random_number).and_return(code)
+      end
 
-        it "selects the country and enters the phone number" do
+      context "when enter email" do
+        it "enters the email" do
           expect(page).to have_current_path(decidim_half_signup.users_quick_auth_email_path)
-          expect(page).to have_link("Another method", href: decidim_half_signup.users_quick_auth_path)
-          click_button "Send code via Email"
+          click_button "SEND THE CODE"
           expect(page).to have_content "There's an error in this field."
           fill_in "Email", with: email
-          click_button "Send code via Email"
+          click_button "SEND THE CODE"
           expect(page).to have_current_path(decidim_half_signup.users_quick_auth_verify_path)
         end
       end
 
       context "when verify" do
         before do
-          click_link "TO YOUR EMAIL"
           fill_in "Email", with: email
-          click_button "Send code via Email"
+          click_button "SEND THE CODE"
         end
 
         it "renders the verify page" do
           expect(page).to have_current_path(decidim_half_signup.users_quick_auth_verify_path)
-          expect(page).to have_content("The verificaiton code is")
           within ".flash" do
             expect(page).to have_content("Verification code sent to someone@example.org.")
           end
-          click_link "Resend code"
+          click_link "Send it again."
           within ".flash" do
             expect(page).not_to have_content("Please wait at least 1 minute to resend the code.")
             expect(page).to have_content("Verification code sent to someone@example.org.")
           end
-          expect(page).to have_link("use another method", href: decidim_half_signup.users_quick_auth_path)
-          code = page.find("#hint").text
-          fill_in "Verification", with: code
+          fill_in_code(code, "digit")
           click_button "Verify"
           expect(page).to have_content "Agree to the terms and conditions of use"
           click_button "I agree with these terms"
           expect(page).to have_current_path("/authorizations")
-          expect(Decidim::User.count).to eq(1)
+          expect(Decidim::User.count).to eq(2)
           user = Decidim::User.last
           expect(user.name).to eq("Unnamed user")
           expect(user.phone_country).to be_nil
@@ -70,14 +68,11 @@ describe "Manage sms sign in", type: :system do
     end
 
     context "when the account exists" do
-      let!(:user) { create(:user, :confirmed, organization: organization, email: "someone@example.org") }
-
       before do
-        click_link "TO YOUR EMAIL"
-        fill_in "Email", with: email
-        click_button "Send code via Email"
-        code = page.find("#hint").text
-        fill_in "Verification", with: code
+        allow(SecureRandom).to receive(:random_number).and_return(code)
+        fill_in "Email", with: "anotherone@example.org"
+        click_button "SEND THE CODE"
+        fill_in_code(code, "digit")
         click_button "Verify"
       end
 
@@ -86,6 +81,44 @@ describe "Manage sms sign in", type: :system do
         expect(Decidim::User.count).to eq(1)
         expect(Decidim::User.last).to eq(user)
       end
+    end
+
+    context "when changing the default auth_code_length" do
+      before do
+        allow(Decidim::HalfSignup).to receive(:auth_code_length).and_return(5)
+        fill_in "Email", with: email
+        click_button "SEND THE CODE"
+      end
+
+      it "generates the correct length code and verifies the user" do
+        expect(all("input[name^='digit']").count).to eq(5)
+      end
+    end
+
+    context "when show_tos_page_after_signup is set to false" do
+      before do
+        allow(SecureRandom).to receive(:random_number).and_return(code)
+        allow(Decidim::HalfSignup).to receive(:show_tos_page_after_signup).and_return(false)
+        fill_in "Email", with: email
+        click_button "SEND THE CODE"
+        fill_in_code(code, "digit")
+        click_button "Verify"
+      end
+
+      it "does not show the agree to the terms page" do
+        expect(Decidim::User.count).to eq(2)
+        user = Decidim::User.last
+        expect(user.name).to eq("Unnamed user")
+        expect(page).to have_current_path("/authorizations")
+      end
+    end
+  end
+
+  private
+
+  def fill_in_code(code, element)
+    code.length.times do |ind|
+      fill_in "#{element}#{ind + 1}", with: code[ind]
     end
   end
 end
