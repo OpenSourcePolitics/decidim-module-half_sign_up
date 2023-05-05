aaa170b0710c927e41653e4f92cddfde40cafde7
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-05-05 13:45:48 +0300

    Fix login popup bug when auth_setting is not enabled

--- spec/system/sign_in_and_sign_up_page_spec.rb
+++ spec/system/sign_in_and_sign_up_page_spec.rb
@@ -6,7 +6,7 @@ describe "Sign in and sign up page", type: :system do
   let(:organization) { create(:organization) }
   let(:user) { create(:user, :confirmed, organization: organization) }
   let(:admin) { create(:user, :admin, :confirmed, organization: organization) }
-  let(:auth_settings) { create(:auth_setting, organization: organization) }
+  let!(:auth_settings) { create(:auth_setting, organization: organization) }
   let(:decidim_half_signup) { Decidim::HalfSignup::Engine.routes.url_helpers }
 
   describe "login" do
@@ -25,6 +25,79 @@ describe "Sign in and sign up page", type: :system do
         expect(page).to have_content("Email")
         expect(page).to have_content("Password")
         expect(page).to have_button("Log in")
+      end
+    end
+
+    context "when modal login pops up" do
+      let(:decidim_budgets) { Decidim::EngineRouter.main_proxy(component) }
+      let!(:component) { create(:budgets_component, organization: organization) }
+      let!(:budget) { create(:budget, component: component) }
+      let!(:projects) { create_list(:project, 3, budget: budget) }
+
+      context "when no auth_setting is enabled" do
+        before do
+          click_a_project
+        end
+
+        it "popups with normal decidim layout" do
+          expect(page).to have_css("#loginModal")
+          within "#loginModal" do
+            expect(page).to have_field("Email")
+            expect(page).to have_field("Password")
+            expect(page).to have_button("Log in")
+          end
+        end
+      end
+
+      context "when email auth is enabled" do
+        before do
+          auth_settings.update!(enable_partial_email_signup: true)
+          click_a_project
+        end
+
+        it "shows email link in the popup" do
+          expect(page).to have_css("#loginModal")
+          within "#loginModal" do
+            expect(page).to have_no_content("Please select how you would like to sign in.")
+            expect(page).to have_content("Please sign in")
+            expect(page).to have_link("With your email", href: "/users/quick_auth/email")
+            expect(page).to have_no_link("With your phone")
+          end
+        end
+      end
+
+      context "when sms auth is enabled" do
+        before do
+          auth_settings.update!(enable_partial_sms_signup: true)
+          click_a_project
+        end
+
+        it "shows email link in the popup" do
+          expect(page).to have_css("#loginModal")
+          within "#loginModal" do
+            expect(page).to have_no_content("Please select how you would like to sign in.")
+            expect(page).to have_content("Please sign in")
+            expect(page).to have_link("With your phone", href: "/users/quick_auth/sms")
+            expect(page).to have_no_link("With your email")
+          end
+        end
+      end
+
+      context "when both are enalbed" do
+        before do
+          auth_settings.update!(enable_partial_sms_signup: true, enable_partial_email_signup: true)
+          click_a_project
+        end
+
+        it "shows email link in the popup" do
+          expect(page).to have_css("#loginModal")
+          within "#loginModal" do
+            expect(page).to have_content("Please select how you would like to sign in.")
+            expect(page).to have_content("Please sign in")
+            expect(page).to have_link("With your phone", href: "/users/quick_auth/sms")
+            expect(page).to have_link("With your email", href: "/users/quick_auth/email")
+          end
+        end
       end
     end
 
@@ -96,5 +169,13 @@ describe "Sign in and sign up page", type: :system do
         expect(page).to have_link("To your email")
       end
     end
+  end
+
+  private
+  def click_a_project
+    visit decidim_budgets.budget_projects_path(budget.id)
+    button = find("#project-vote-button-#{projects.first.id}")
+    page.execute_script("arguments[0].removeAttribute('disabled')", button)
+    button.click
   end
 end
