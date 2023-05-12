bf6064a7ee349d50f20d0d8a0e986aeff9513f13
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-05-12 16:42:33 +0300

    Make auto-complete off for the slimSelect

--- app/packs/src/decidim/half_signup/select_country.js
+++ app/packs/src/decidim/half_signup/select_country.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
       data.push({
         text: opt.textContent,
         value: opt.value,
+        autocomplete: "off",
         selected: opt.selected,
         innerHTML: `<span class="country-flag"><img src="${opt.dataset.flagImage}" loading="lazy" alt=""></span><span class="country-text">${opt.textContent}</span>`
       });
