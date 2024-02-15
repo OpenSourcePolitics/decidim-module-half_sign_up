3b03df5ad115f8ca7ebd2049efdaac86a3cb41c1
AyakorK <guillaume.moret@yahoo.com>
2024-02-15 17:49:50 +0100

    feat: Readd capybara and import it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
 require "decidim/dev"
+require "capybara"
 
 require "simplecov"
 SimpleCov.start
+
+Capybara.register_driver :headless_chrome do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.args << "--headless=new"
+  options.args << "--no-sandbox"
+  options.args << if ENV["BIG_SCREEN_SIZE"].present?
+                    "--window-size=1920,3000"
+                  else
+                    "--window-size=1920,1080"
+                  end
+  options.args << "--ignore-certificate-errors" if ENV["TEST_SSL"]
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    capabilities: [options]
+  )
+end
 
 ENV["ENGINE_ROOT"] = File.dirname(__dir__)
 
