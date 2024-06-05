85c717d5a645c55a7d313c480de7dae4a5bd52d8
barbara oliveira <barbarafictiovore@gmail.com>
2024-06-05 16:19:10 +0200

    display error message when unvalid number

--- app/packs/src/decidim/half_signup/phone_validation.js
+++ app/packs/src/decidim/half_signup/phone_validation.js
@@ -1,28 +1,23 @@
 $(() => {
-    console.log("Hello")
-    console.log($("#sms_auth_phone_country").val())
-
     const updateButtonState = () => {
+
         if ($("#sms_auth_phone_country").val() === "FR") {
             $(".button").prop("disabled", true);
-            console.log("change to FR works");
 
             $("#sms_auth_phone_number").on('input', function() {
                 const phoneNumber = $(this).val();
                 if ((phoneNumber.charAt(0) === "0" && phoneNumber.length === 10) || (phoneNumber.charAt(0) !== "0" && phoneNumber.length === 9)) {
-                    console.log(phoneNumber);
-                    console.log(phoneNumber.charAt(0));
-                    console.log(typeof (phoneNumber));
-                    console.log(phoneNumber.length);
-                    $(".button").prop("disabled", false);
                     const regex = /^(0[67]|[67])\d{8}$/;
 
                     if (regex.test(phoneNumber)) {
                         console.log("valid number");
-                        // $(".button").prop("disabled", false);
+                        $(".button").prop("disabled", false);
                     } else {
                         console.log("unvalid number");
-                        $('#form-error').show();
+                        const phoneNumberError = $("#sms_auth_phone_number").siblings("span.form-error");
+                        console.log(phoneNumberError)
+                        console.log(phoneNumberError.addClass('is-visible'))
+                        phoneNumberError.addClass('is-visible').html('Is not valid, it must start with 06 or 07 and contain 10 digits');
                     }
                 } else {
                     $(".button").prop("disabled", true);
@@ -31,6 +26,7 @@ $(() => {
 
         } else {
                     $(".button").prop("disabled", false);
+                    $("#sms_auth_phone_number").off('input');
                 }
     };
 
