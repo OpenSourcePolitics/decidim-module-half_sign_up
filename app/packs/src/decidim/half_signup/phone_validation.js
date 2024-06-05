9145df79ca9a663b13ec312c48661ce5e0721c76
barbara oliveira <barbarafictiovore@gmail.com>
2024-06-05 12:01:47 +0200

    feat:validations works with FR default or not

--- app/packs/src/decidim/half_signup/phone_validation.js
+++ app/packs/src/decidim/half_signup/phone_validation.js
@@ -5,26 +5,15 @@ $(() => {
     const updateButtonState = () => {
         if ($("#sms_auth_phone_country").val() === "FR") {
             $(".button").prop("disabled", true);
-        } else {
-            $(".button").prop("disabled", false);
-        }
-    }
+            console.log("change to FR works");
 
-    updateButtonState()
-
-    $("#sms_auth_phone_country").change( function(){
-        console.log($("#sms_auth_phone_country").val())
-
-        if ($("#sms_auth_phone_country").val() === "FR") {
-            console.log("change to FR works")
-            $(".button").prop("disabled", true);
-            $("#sms_auth_phone_number").on('input', function(){
+            $("#sms_auth_phone_number").on('input', function() {
                 const phoneNumber = $(this).val();
                 if ((phoneNumber.charAt(0) === "0" && phoneNumber.length === 10) || (phoneNumber.charAt(0) !== "0" && phoneNumber.length === 9)) {
-                    console.log(phoneNumber)
-                    console.log(phoneNumber.charAt(0))
-                    console.log(typeof(phoneNumber))
-                    console.log(phoneNumber.length)
+                    console.log(phoneNumber);
+                    console.log(phoneNumber.charAt(0));
+                    console.log(typeof (phoneNumber));
+                    console.log(phoneNumber.length);
                     $(".button").prop("disabled", false);
                     const regex = /^(0[67]|[67])\d{8}$/;
 
@@ -35,12 +24,22 @@ $(() => {
                         console.log("unvalid number");
                         $('#form-error').show();
                     }
+                } else {
+                    $(".button").prop("disabled", true);
                 }
+            });
 
-            })
-    }
+        } else {
+                    $(".button").prop("disabled", false);
+                }
+    };
+
+    updateButtonState()
+
+    $("#sms_auth_phone_country").change( function(){
+        console.log($("#sms_auth_phone_country").val())
+
+        updateButtonState()
     })
-
-
-    })
+})
 
