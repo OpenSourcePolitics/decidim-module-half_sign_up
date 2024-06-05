df2a6ec7cec32b5d4150eb9be079ea2c481a8ab4
barbara oliveira <barbarafictiovore@gmail.com>
2024-06-05 18:36:38 +0200

    add key trad

--- app/packs/src/decidim/half_signup/phone_validation.js
+++ app/packs/src/decidim/half_signup/phone_validation.js
@@ -1,7 +1,7 @@
 $(() => {
     const updateButtonState = () => {
 
-        if ($("#sms_auth_phone_country").val() === "FR") {
+        if ($("#sms_auth_phone_country").val() === "FR" ) {
             $(".button").prop("disabled", true);
 
             $("#sms_auth_phone_number").attr("placeholder", "0621212121");
@@ -9,18 +9,18 @@ $(() => {
             $("#sms_auth_phone_number").on('input', function() {
                 const phoneNumber = $(this).val();
                 if ((phoneNumber.charAt(0) === "0" && phoneNumber.length === 10) || (phoneNumber.charAt(0) !== "0" && phoneNumber.length === 9)) {
-                    const regex = /^(0[67]|[67])\d{8}$/;
 
+                    const regex = /^(0[67]|[67])\d{8}$/;
+                    const phoneNumberError = $("#sms_auth_phone_number").siblings("span.form-error");
                     if (regex.test(phoneNumber)) {
-                        console.log("valid number");
+
+                        phoneNumberError.addClass('is-visible').html($("#phone_number_invalid_message").attr("hidden", true));
                         $(".button").prop("disabled", false);
+
                     } else {
-                        console.log("unvalid number");
-                        const phoneNumberError = $("#sms_auth_phone_number").siblings("span.form-error");
-                        console.log(phoneNumberError)
-                        console.log(phoneNumberError.addClass('is-visible'))
-                        phoneNumberError.addClass('is-visible').html('Is not valid, it must start with 06 or 07 and contain 10 digits');
+                        phoneNumberError.addClass('is-visible').html($("#phone_number_invalid_message").text());
                     }
+
                 } else {
                     $(".button").prop("disabled", true);
                 }
