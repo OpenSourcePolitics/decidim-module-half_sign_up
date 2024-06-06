f2ece40beae0a885ccde7e0b30f2aa3d94600400
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-06-06 16:55:23 +0200

    revert: Authenticate user comman

--- app/commands/decidim/half_signup/authenticate_user.rb
+++ app/commands/decidim/half_signup/authenticate_user.rb
@@ -13,9 +13,7 @@ module Decidim
         return broadcast(:invalid, verification_failed) unless validate!
 
         user = find_or_create_user!
-        return broadcast(:ok, user) if user.present?
-
-        broadcast(:invalid, I18n.t("error", scope: "decidim.half_signup.quick_auth.authenticate_user"))
+        broadcast(:ok, user)
       end
 
       private
@@ -39,58 +37,39 @@ module Decidim
       end
 
       def find_or_create_user!
-        ActiveRecord::Base.transaction do
-          user = if sms_auth?
-                   if session.present? && session[:user_id].present?
-                     existing_user = update_decidim_user_phone(session, data)
+        user = if sms_auth?
+                 if session.present? && session[:user_id].present?
+                   existing_user = update_decidim_user_phone(session, data)
 
-                     existing_user.presence || find_user_by_phone_country(data)
-                   else
-                     find_user_by_phone_country(data)
-                   end
+                   existing_user.presence || find_user_by_phone_country(data)
                  else
-                   Decidim::User.find_by(
-                     email: data["email"],
-                     organization: form.organization
-                   )
+                   find_user_by_phone_country(data)
                  end
+               else
+                 Decidim::User.find_by(
+                   email: data["email"],
+                   organization: form.organization
+                 )
+               end
 
-          return user if user.present?
+        return user if user.present?
 
-          generated_password = SecureRandom.hex
+        generated_password = SecureRandom.hex
+        Decidim::User.create! do |record|
+          record.name = I18n.t("unnamed_user", scope: "decidim.half_signup.quick_auth.authenticate")
+          record.nickname = UserBaseEntity.nicknamize(record.name)
+          record.email = data["email"] || generate_email(data["country"], data["phone"])
+          record.password = generated_password
+          record.password_confirmation = generated_password
 
-          # Use a database lock to ensure atomicity
-          Decidim::User.with_lock do
-            user = if sms_auth?
-                     find_user_by_phone_country(data)
-                   else
-                     Decidim::User.find_by(
-                       email: data["email"],
-                       organization: form.organization
-                     )
-                   end
+          record.skip_confirmation!
 
-            return user if user.present?
-
-            Decidim::User.create! do |record|
-              record.name = I18n.t("unnamed_user", scope: "decidim.half_signup.quick_auth.authenticate")
-              record.nickname = UserBaseEntity.nicknamize("#{record.name}_#{rand(1000)}")
-              record.email = data["email"].presence || generate_email(data["country"], data["phone"])
-              record.password = generated_password
-              record.password_confirmation = generated_password
-
-              record.skip_confirmation!
-
-              record.phone_number = data["phone"]
-              record.phone_country = data["country"]
-              record.tos_agreement = "1"
-              record.organization = form.organization
-              record.accepted_tos_version = Time.current unless Decidim::HalfSignup.show_tos_page_after_signup
-              record.locale = form.current_locale
-            end
-          end
-        rescue ActiveRecord::RecordInvalid
-          nil
+          record.phone_number = data["phone"]
+          record.phone_country = data["country"]
+          record.tos_agreement = "1"
+          record.organization = form.organization
+          record.accepted_tos_version = Time.current unless Decidim::HalfSignup.show_tos_page_after_signup
+          record.locale = form.current_locale
         end
       end
 
