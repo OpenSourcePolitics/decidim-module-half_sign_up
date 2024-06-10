ac5fe2db209a94d3e7c68a1b5130cbbbe1fbbef3
barbara oliveira <barbarafictiovore@gmail.com>
2024-06-10 17:23:28 +0200

    fix rspec

--- app/forms/decidim/half_signup/sms_auth_form.rb
+++ app/forms/decidim/half_signup/sms_auth_form.rb
@@ -9,7 +9,18 @@ module Decidim
       attribute :phone_country, String
 
       validates :phone_number, numericality: { greater_than: 0 }, presence: true
+      validates :phone_number, presence: true, numericality: { greater_than: 0 }
+      validate :validate_phone_number_format, if: -> { phone_country == "FR" }
       validates :phone_country, presence: true
+
+      private
+
+      def validate_phone_number_format
+        phone_number_str = phone_number.to_s
+        return if phone_number_str.match?(/\A(0[67]|[67])\d{8}\z/)
+
+        errors.add(:phone_number, "Is not valid, it must start with 06 or 07 and contain 10 digits")
+      end
     end
   end
 end
