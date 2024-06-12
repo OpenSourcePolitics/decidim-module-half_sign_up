0df69422948f6edb1b48cc4cc766692204796ff6
barbara oliveira <barbarafictiovore@gmail.com>
2024-06-12 14:17:31 +0200

    fix rspec Finland => Andorra

--- spec/system/budgets_view_spec.rb
+++ spec/system/budgets_view_spec.rb
@@ -40,7 +40,7 @@ describe "Budgets view", type: :system do
         context "when user fills half signup sms form" do
           before do
             find("a.hollow:nth-child(1)").click
-            select "Finland", from: :sms_auth_phone_country
+            select "Andorra", from: :sms_auth_phone_country
             fill_in :sms_auth_phone_number, with: "4578878784"
             click_button "Send the code"
           end
@@ -78,7 +78,7 @@ describe "Budgets view", type: :system do
           context "when user fills half signup sms form" do
             before do
               find("button.hollow:nth-child(1)").click
-              select "Finland", from: :sms_auth_phone_country
+              select "Andorra", from: :sms_auth_phone_country
               fill_in :sms_auth_phone_number, with: "4578878784"
               click_button "Send the code"
             end
