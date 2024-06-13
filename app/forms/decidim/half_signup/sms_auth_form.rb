3902b2adc262ed1ff3f49786a6cbd15c7c21c235
barbara oliveira <barbarafictiovore@gmail.com>
2024-06-13 10:24:00 +0200

    add trad key

--- app/forms/decidim/half_signup/sms_auth_form.rb
+++ app/forms/decidim/half_signup/sms_auth_form.rb
@@ -18,7 +18,7 @@ module Decidim
         phone_number_str = phone_number.to_s
         return if phone_number_str.match?(/\A(0[67]|[67])\d{8}\z/)
 
-        errors.add(:phone_number, "Is not valid, it must start with 06 or 07 and contain 10 digits")
+        errors.add(:phone_number, I18n.t("decidim.half_signup.quick_auth.sms.invalid_phone"))
       end
     end
   end
